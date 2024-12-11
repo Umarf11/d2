046150d5eb11af9215c325c32d5c26ab53795934
Umarf11 <umardevtech11@gmail.com>
2024-12-11 18:36:08 +0500

    Generics

--- employees/views.py
+++ employees/views.py
@@ -56,26 +56,50 @@ from rest_framework import mixins, generics
 #----------------------------------------------------------------------------End-------------------------------------------------------------------------------
 
 #----------------------------------------------------------------------------Mixins---------------------------------------------------------------------------------------
-class Employees(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
+# class Employees(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+#     queryset = Employee.objects.all()
+#     serializer_class = EmployeeSerializer
 
-    def get(self, request):
-        return self.list(request)
+#     def get(self, request):
+#         return self.list(request)
 
-    def post(self, request):
-        return self.create(request)
+#     def post(self, request):
+#         return self.create(request)
 
 
-class EmployeeDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
+# class EmployeeDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+#     queryset = Employee.objects.all()
+#     serializer_class = EmployeeSerializer
 
-    def get(self, request, pk):
-        return self.retrieve(request, pk)
+#     def get(self, request, pk):
+#         return self.retrieve(request, pk)
 
-    def put(self, request, pk):
-        return self.update(request, pk)
+#     def put(self, request, pk):
+#         return self.update(request, pk)
 
-    def delete(self, request, pk):
-        return self.destroy(request, pk)
+#     def delete(self, request, pk):
+#         return self.destroy(request, pk)    
+
+#-----------------------------------------------------------------------End---------------------------------------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------Generics----------------------------------------------------------------------------------------------------------------------------
+#SEPERATE..........
+# class Employees(generics.ListAPIView, generics.CreateAPIView):
+#     queryset = Employee.objects.all()
+#     serializer_class = EmployeeSerializer
+
+#COMBINE.........
+# class Employees(generics.ListCreateAPIView):
+#     queryset = Employee.objects.all()
+#     serializer_class = EmployeeSerializer
+
+#SEPERATE..........
+# class EmployeeDetail(generics.RetrieveAPIView, generics.UpdateAPIView, generics.DestroyAPIView):
+#     queryset = Employee.objects.all()
+#     serializer_class = EmployeeSerializer
+
+
+#COMBINE.............
+# class EmployeeDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Employee.objects.all()
+#     serializer_class = EmployeeSerializer
